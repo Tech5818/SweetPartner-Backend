2c25ea6b780ac84e5914b4b4475c8514720b9324
Tech5818 <pqow5818@gmail.com>
2024-05-13 23:48:08 +0900

    :sparkles: docker 설정

--- main.py
+++ main.py
@@ -12,9 +12,13 @@ app = FastAPI()
 app.include_router(user_router, prefix="/user")
 app.include_router(google_oauth_router, prefix="/auth/google", tags=["auth"])
 
+@app.get("/")
+async def home():
+  return "home"
+
 @app.on_event("startup")
 async def on_startup():
   await connect_mongodb()
 
 if __name__ == "__main__":
-  uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+  uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
--- models/user.py
+++ models/user.py
@@ -1,7 +1,6 @@
 from beanie import Document
 from typing import Optional
 from fastapi_users.db import BeanieBaseUser
-
 class User(BeanieBaseUser, Document):
   name: str
   password: str
